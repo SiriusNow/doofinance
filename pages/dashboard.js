f97588a2422e63a2330eabe300e30da5de6eeda9
SiriusNow <munhodg@gmail.com>
2022-12-19 00:25:21 +0800

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -171,23 +171,23 @@ export default function Dashboard() {
   useEffect(() => {
     const _id = localStorage.getItem("id");
     setLoading(true);
-    fetch("http://doofinanceserver.vercel.app/list/" + _id).then(
-      async (res) => {
-        let response = await res.json();
-        setData(response);
-        setDatas(dataD(response, true));
-        setDatas1(dataD(response, false));
-        setBarChart(
-          chartData(response, startDate.getDate() + 1, startDate.getDate() - 8)
-        );
-        // response.forEach((e)=>{
-        //   datas.datasets[0].data.push(e.value);
-        //   datas.labels.push(e.type);
-        // })
-        setLoading(false);
-        return response;
-      }
-    );
+    fetch("http://doofinanceserver.vercel.app/list/" + _id, {
+      referrerPolicy: "unsafe_url",
+    }).then(async (res) => {
+      let response = await res.json();
+      setData(response);
+      setDatas(dataD(response, true));
+      setDatas1(dataD(response, false));
+      setBarChart(
+        chartData(response, startDate.getDate() + 1, startDate.getDate() - 8)
+      );
+      // response.forEach((e)=>{
+      //   datas.datasets[0].data.push(e.value);
+      //   datas.labels.push(e.type);
+      // })
+      setLoading(false);
+      return response;
+    });
   }, [checkPost]);
   const createList = () => {
     if (type != "" && value > 0 && category != "") {
